a82b539e193e58f9bd94bdc91a5cb38f0bde1a82
toshi <k08045kk@gmail.com>
2021-06-03 00:00:00 +0900

    fix セパレータを編集できる

--- BookmarkbarNotEditable.uc.js
+++ BookmarkbarNotEditable.uc.js
@@ -4,45 +4,32 @@
 //              Shiftキー押下+ドラッグで編集可能とする。
 // @include     main
 // @charset     UTF-8
-// @version     0.2
+// @version     0.3
 // @since       0.1 - 20210521 - 初版
 // @since       0.2 - 20210524 - ドラッグ時のブックマーククリックを無効化
+// @since       0.3 - 20210603 - fix セパレータを編集できる
 // ==/UserScript==
 
 (function() {
-  // 親要素を判定
-  const checkParentElement = function(element, id) {
-    for (; element; element=element.parentElement) {
-      if (element.id == id) {
-        return true;
-      }
-    }
-    return false;
-  };
+  const toolbar = document.getElementById('PlacesToolbar');
+  const items = ['toolbarbutton','toolbarseparator','menupopup','menu','menuitem','menuseparator'];
+  let isInterrup;
   
-  // ドラッグ開始（編集開始）を無効化
-  document.addEventListener('dragstart', function(e) {
-    if (e.target 
-     && !e.shiftKey
-     && checkParentElement(e.target, 'PlacesToolbar') 
-     && ~['menuitem','menu','toolbarbutton'].indexOf(e.target.tagName)) {
+  // ドラッグ開始を無効化（Shift+ドラッグ開始は、有効とする）
+  toolbar.addEventListener('dragstart', function(e) {
+    if (!e.shiftKey && ~items.indexOf(e.target.tagName)) {
+      isInterrup = true;
       e.preventDefault();
     }
   }, true);
   
-  // ドラッグ時（編集不可時）のブックマーククリックを無効化
-  let x = 0;
-  let y = 0;
-  document.addEventListener('mousedown', function(e) {
-    x = e.clientX;
-    y = e.clientY;
+  // ドラッグ時のブックマーククリックを無効化
+  toolbar.addEventListener('mousedown', function(e) {
+    isInterrup = false;
   });
-  document.addEventListener('mouseup', function(e) {
-    if (e.clientX != x && e.clientY != y
-     && e.target 
-     && checkParentElement(e.target, 'PlacesToolbar') 
-     && ~['menuitem','menu','toolbarbutton'].indexOf(e.target.tagName)) {
+  toolbar.addEventListener('mouseup', function(e) {
+    if (isInterrup) {
       e.preventDefault();
     }
-  }, true);
+  });
 })();
