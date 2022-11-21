59b97f12649f4f32d300f2b209e53f067093580f
toshi <k08045kk@gmail.com>
2022-11-21 20:31:44 +0900

    二版（#downloadsPanel 方式を追加）

--- DisplayNumberOfDownloadsOnButton.uc.js
+++ DisplayNumberOfDownloadsOnButton.uc.js
@@ -7,8 +7,9 @@
 // @author        toshi (https://github.com/k08045kk)
 // @license       MIT License | https://opensource.org/licenses/MIT
 // @compatibility 106+
-// @version       1
+// @version       2
 // @since         1 - 20221107 - 初版
+// @since         2 - 20221121 - 二版（#downloadsPanel 方式を追加）
 // @see           https://github.com/k08045kk/userChrome.js
 // ==/UserScript==
 
@@ -37,6 +38,10 @@ window.addEventListener('load', function() {
   // バッジを変更する
   const label = button.querySelector('.toolbarbutton-badge');
   const setBadgeText = function(text) {
+    const attention = button.getAttribute('attention');
+    if (text === 0 || attention !== '') {
+      text = '';
+    }
     if (text != label.textContent) {
       label.textContent = text;
       label.style.backgroundColor = text !== '' ? 'blueviolet' : '';
@@ -45,6 +50,7 @@ window.addEventListener('load', function() {
     //       attention=info 危険なファイルのダウンロード（丸アイコン）
     //       attention=warning 不明（三角アイコン）
     //       attention=severe 不明（丸アイコン）
+    //       テキストではなく、図形で表される
   };
   
   
@@ -54,7 +60,6 @@ window.addEventListener('load', function() {
   const callback = function(mutationsList, observer) {
     const progress = button.getAttribute('progress');
     const notification = button.getAttribute('notification');
-    const attention = button.getAttribute('attention');
     if (progress === 'true') {
       if (_notification === notification) {
         if (share.data.count === 0) {
@@ -78,22 +83,32 @@ window.addEventListener('load', function() {
       }
       _notification = notification;
     } else {
+      // ありえない（といいな）
       share.data.count = 0;
     }
     _progress = progress;
-    if (attention === '') {
-      setBadgeText(share.data.count || '');
-    } else {
-      setBadgeText('');
-    }
+    setBadgeText(share.data.count);
     
-    //console.log(windowId, share.data.count, progress, notification, attention);
+    //console.log(windowId, share.data.count, progress, notification);
     // 補足：start / finish が 同時 or 連続 に発生するとカウントがズレる。
     // 補足：ダウンロード失敗が発生るとカウントがズレる。
   };
   const config = {attributes:true, childList:false, subtree:false};
   const observer = new MutationObserver(callback);
   observer.observe(button, config);
+  
+  
+  // ダウンロードパネルで補正する
+  button.addEventListener('click', function(event) {
+    const downloads = document.querySelectorAll('#downloadsListBox > .download-state[state="0"]');
+    const summary = document.getElementById('downloadsSummaryDescription');
+    if (summary.value) {
+      share.data.count = downloads.length + (summary.value.match(/\d+/)[0] - 0);
+      setBadgeText(share.data.count);
+    }
+    
+    // 補足：初回クリック時まではダウンロードパネルが未初期化
+  });
   
   // 補足：設置済みでも load 前には、ダウンロードボタンが存在しない可能性がある。
   // 補足：「about:downloads」から取得すれば正確なダウンロード数が取得できる。
