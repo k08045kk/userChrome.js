0f10c9e7f3647cadd703a55472ea5809f122bc44
toshi <k08045kk@gmail.com>
2024-05-27 08:05:52 +0900

    ページ遷移時は、オートコピーしない

--- URLBarAutoCopy.uc.js
+++ URLBarAutoCopy.uc.js
@@ -7,8 +7,9 @@
 // @author        toshi (https://github.com/k08045kk)
 // @license       MIT License | https://opensource.org/licenses/MIT
 // @compatibility 101+
-// @version       0.1
+// @version       0.2
 // @since         0.1 - 20220610 - 初版
+// @since         0.2 - 20240527 - ページ遷移時は、オートコピーしない
 // ==/UserScript==
 
 (function() {
@@ -17,15 +18,18 @@
   // URLバーの内容をクリップボードにコピーする
   const urlbar = document.getElementById('urlbar-input');
   let clipboard = null;
+  let url = null;
   
   urlbar.addEventListener('focus', async function(event) {
     clipboard = await navigator.clipboard.readText();
+    url = gBrowser.selectedBrowser.currentURI.spec;
   });
   
   urlbar.addEventListener('blur', async function(event) {
     const target = event.target;
     const text = target.value.substring(target.selectionStart, target.selectionEnd);
-    if (text.length && clipboard === await navigator.clipboard.readText()) {
+    const viewURL = gBrowser.selectedBrowser.currentURI.spec;
+    if (viewURL === url && text.length && clipboard === await navigator.clipboard.readText()) {
       await navigator.clipboard.writeText(text);
     }
   });
